aec0ff08c3a582e131158ccdac13eabf6ae63984
hanyongjun <gksdydwns34@naver.com>
2023-03-15 00:26:46 +0900

    각 페이지의 url를 제어해주는 Controller

---
+++ src/main/java/com/inhatc/cs/HomeController.java
@@ -0,0 +1,60 @@
+package com.inhatc.cs;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller // *mvc 중 어노테이션에 속함
+public class HomeController { 
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET) // 사용자의 요청을 매핑했을 때 사용자의 요청을 매핑하는 어노테이션
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home"; //
+	}
+	
+	@RequestMapping(value = "/gugudan", method = RequestMethod.GET) // 사용자의 요청을 매핑했을 때 사용자의 요청을 매핑하는 어노테이션
+	public String gugudan() {	
+		return "gugudan"; //
+	}
+	
+	@RequestMapping(value = "/1to10", method = RequestMethod.GET) // 사용자의 요청을 매핑했을 때 사용자의 요청을 매핑하는 어노테이션
+	public String Ototen() {	
+		return "1to10"; //
+	}
+	
+	@RequestMapping(value = "/lotto", method = RequestMethod.GET) // 사용자의 요청을 매핑했을 때 사용자의 요청을 매핑하는 어노테이션
+	public String lotto() {	
+		return "lotto"; //
+	}
+	
+	@RequestMapping(value = "/sosu", method = RequestMethod.GET) // 사용자의 요청을 매핑했을 때 사용자의 요청을 매핑하는 어노테이션
+	public String sosu() {	
+		return "sosu"; //
+	}
+	
+	
+}
